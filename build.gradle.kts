8499b57b7be01dd1e0339627e372ef73764b546e
MASKUN2 <maskunflower2@naver.com>
2026-04-27 10:09:53 +0900

    feat: remove consul dependency for k8s migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.cloud:spring-cloud-starter-consul-discovery")
-    implementation("org.springframework.cloud:spring-cloud-starter-consul-config")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
